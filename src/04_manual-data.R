f84f3fddc8b4ad82d1bcb3b9f048d15e280f4e0d
bgulbis <brian.gulbis@gmail.com>
2017-09-20 13:39:25 -0500

    add identifiers to manual data

--- src/04_manual-data.R
+++ src/04_manual-data.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(readxl)
 library(stringr)
+library(edwr)
+
+identifiers <- read_data("data/raw", "identifiers") %>%
+    as.id
 
 data_manual_rk <- read_excel("data/external/manual_data_rk.xlsx") %>%
     select(-`ACCESS DATE`) %>%
@@ -14,7 +18,10 @@ data_manual_rk <- read_excel("data/external/manual_data_rk.xlsx") %>%
               str_replace_all,
               pattern = "\\?",
               replacement = "") %>%
-    mutate_at(c("afib", "new_oac", "revisit_anticoag_related"), as.logical)
+    mutate_at(c("afib", "new_oac", "revisit_anticoag_related"), as.logical) %>%
+    mutate_at("fin", as.character) %>%
+    left_join(identifiers, by = "fin") %>%
+    select(millennium.id, afib:exclude)
 
 data_manual_bs <- read_excel("data/external/manual_data_bs.xlsx") %>%
     mutate_at(c("afib", "dvt", "pe", "other", "new_oac",
@@ -23,7 +30,10 @@ data_manual_bs <- read_excel("data/external/manual_data_bs.xlsx") %>%
     mutate_at("other", funs(. != "FALSE")) %>%
     mutate_at(c("afib", "dvt", "pe", "new_oac", "revisit_anticoag_related",
                 "new_bleed", "new_thrombus"),
-              as.logical)
+              as.logical) %>%
+    mutate_at("fin", as.character) %>%
+    left_join(identifiers, by = "fin") %>%
+    select(millennium.id, afib:notes)
 
 write_rds(data_manual_rk, "data/tidy/manual_rk.Rds", "gz")
 write_rds(data_manual_bs, "data/tidy/manual_bs.Rds", "gz")
